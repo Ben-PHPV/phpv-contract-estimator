29685aaf1ad38d92ff9b69765f93d378d52beaef
Ben-PHPV <benjamin.p.scanlon@gmail.com>
2025-05-23 16:13:32 -0600

    Update app.py

--- static/app.py
+++ static/app.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+@app.route("/")
+def serve_estimator():
+    return send_from_directory(app.static_folder, "index.html")
 
 @app.route('/')
 def home():
